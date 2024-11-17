6e2a26be1196dfd2c103f2457a62da38f67a095e
Julio Errecart <julioer@me.com>
2024-11-17 17:14:02 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+print("Portfolio #1: Day 82 Start")
+
+morse_code = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": " ",
+}
+
+message = input("Enter a message to translate to Morse Code: ").upper()
+output = ""
+
+for i in range(0,len(message)):
+    # print(message[i])
+    # print(morse_code[message[i]])
+    output += morse_code[message[i]] + " "
+
+print(f"You entered: {message}")
+print(f"Morse Code Translation: {output}")
